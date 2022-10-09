6c8fb15bf58e4bb6813e62fa7adbd396e55ae668
Adarsh <Adarsh@DESKTOP-A0PA6AD>
2022-10-09 19:56:05 +0530

    222222222222

---
+++ src/test/java/scrpts/testcase4.java
@@ -0,0 +1,14 @@
+package scrpts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class testcase4 {
+	public class testcase1 {
+
+		@Test
+		public void tc1() {
+			Reporter.log("hello world",true);
+		}
+	}
+}
